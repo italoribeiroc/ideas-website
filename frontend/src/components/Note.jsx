e3431c01db0249339d85c837c2d30cd3dedcbbb5
italo <italovianelli59@hotmail.com>
2024-05-13 23:53:02 -0300

    First commit

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/Note.css";
+
+function Note({ note, onDelete }) {
+    const formttedDate = new Date(note.created_at).toLocaleDateString("en-US");
+    return (
+        <div className="note-container">
+            <p className="note-title">{note.title}</p>
+            <p className="note-content">{note.content}</p>
+            <p className="note-date">{formttedDate}</p>
+            <button className="delete-button" onClick={() => onDelete(note.id)} type="button">
+                Delete
+            </button>
+        </div>
+    )
+}
+
+export default Note;
